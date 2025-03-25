51d3e88800467e42bb9c18866f7b70086261728a
Nikhil <130372520+nikhilt77@users.noreply.github.com>
2025-03-25 08:47:52 +0530

    Update ftpserver.c

--- ftpserver.c
+++ ftpserver.c
@@ -1,28 +1,75 @@
 #include<stdio.h>
+#include<unistd.h>
+#include<stdlib.h>
 #include<arpa/inet.h>
 #include<string.h>
-#include<stdlib.h>
-#include<unistd.h>
+
+#define BUFFER_SIZE 1024
+#define PORT 8080
+
+void handleClient(int client_socket){
+
+    char filename[BUFFER_SIZE];
+    char buffer[BUFFER_SIZE];
+
+    int bytesReceived=recv(client_socket,filename,BUFFER_SIZE,0);
+
+    if(bytesReceived<0){
+        perror("File not received\n");
+        close(client_socket);
+        return;
+    }
+
+    FILE* file=fopen(filename,"r");
+    if(file==NULL){
+        char* errorMessage="Failed to load file";
+        send(client_socket,errorMessage,strlen(errorMessage),0);
+        perror("File not found\n");
+        return;
+    }
+    else{
+        while(fgets(buffer,BUFFER_SIZE,file)!=NULL){
+            send(client_socket,buffer,BUFFER_SIZE,0);
+            memset(buffer,0,BUFFER_SIZE);
+        }
+
+        printf("File sent to client\n");
+        fclose(file);
+    }
+    close(client_socket);
+}
 
 int main(){
-    int sockfd,newsock;
-    struct sockaddr_in server,client;
-    socklen_t len=sizeof(client);
-    char buffer[1024];
+    struct sockaddr_in server_addr,client_addr;
+    int server_socket,client_socket;
+    socklen_t len=sizeof(client_addr);
 
-    sockfd=socket(AF_INET,SOCK_STREAM,0);
+    server_socket=socket(AF_INET,SOCK_STREAM,0);
+    if(server_socket<0){
+        perror("Server socket creation failed\n");
+        exit(EXIT_FAILURE);
+    }
+    server_addr.sin_family=AF_INET;
+    server_addr.sin_addr.s_addr=INADDR_ANY;
+    server_addr.sin_port=htons(PORT);
 
-    server.sin_family=AF_INET;
-    server.sin_addr.s_addr=INADDR_ANY;
-    server.sin_port=htons(8080);
+    if(bind(server_socket,(struct sockaddr*)&server_addr,sizeof(server_addr))<0){
+        perror("Binding Failed\n");
+        exit(EXIT_FAILURE);
+    }
 
-    bind(sockfd,(struct sockaddr*)&server,sizeof(server));
-    listen(sockfd,5);
-    newsock=accept(sockfd,(struct sockaddr*)&client,&len);
-    read(newsock,buffer,sizeof(buffer));
-    printf("Message received from client:%s\n",buffer);
-    send(newsock,"Hello from server we are now connected\n",40,0);
+    if(listen(server_socket,3)<0){
+        perror("Listening Rejected\n");
+        exit(EXIT_FAILURE);
+    }
 
-    close(newsock);
-    close(sockfd);
+    while(1){
+        client_socket=accept(server_socket,(struct sockaddr*)&client_addr,&len);
+        if(client_socket<0){
+            perror("Failed to accept the client\n");
+            exit(EXIT_FAILURE);
+        }
+        handleClient(client_socket);
+    }
+    close(server_socket);
 }
